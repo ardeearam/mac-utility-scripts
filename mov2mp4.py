677b078df1b9ec801ac8d1084f3b1a8a301329b5
Ardee Aram <ardee.aram@gmail.com>
2024-11-11 07:45:43 +0800

    Catch if the user did not provide any parameters

--- mov2mp4.py
+++ mov2mp4.py
@@ -3,9 +3,12 @@
 import sys
 import subprocess
 
-old_filename = sys.argv[1]
-uuidgen_process = subprocess.run(['uuidgen'], check=True, text=True, stdout=subprocess.PIPE)
-new_filename = uuidgen_process.stdout.strip()
-print(old_filename)
-print(new_filename)
-ffmpeg_process = subprocess.run(f'ffmpeg -i "{old_filename}.mov" {new_filename}.mp4 && rm "{old_filename}.mov"', shell=True, check=True, text=True)
+try:
+  old_filename = sys.argv[1]
+  uuidgen_process = subprocess.run(['uuidgen'], check=True, text=True, stdout=subprocess.PIPE)
+  new_filename = uuidgen_process.stdout.strip()
+  print(old_filename)
+  print(new_filename)
+  ffmpeg_process = subprocess.run(f'ffmpeg -i "{old_filename}.mov" {new_filename}.mp4 && rm "{old_filename}.mov"', shell=True, check=True, text=True)
+except IndexError:
+  print('Usage: mov2mp4.py "screenshot 2024-11-11 at 7.39.33 AM"\n')
